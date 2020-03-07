730d2c0632ac7717b44e2f1edba0eb444a8fb3bb
Peter Simons <peter.simons@solarwinds.com>
2020-03-07 22:48:09 +0000

    Adds all yellow words

--- Descriptionary/GameData/DescribeMeLists.cs
+++ Descriptionary/GameData/DescribeMeLists.cs
@@ -5,9 +5,59 @@ namespace Descriptionary.GameData
 {
     public class DescribeMe
     {
-
-
-        readonly string[] yellowList = { "Furry", "Friendly", "Wild", "Quirky", "Mushy", "Awkward" };
+        readonly string[] yellowList =
+        {
+                "Awkward",
+                "Calm",
+                "Cheerful",
+                "Chunky",
+                "Clumsy",
+                "Colossal",
+                "Complex",
+                "Cool",
+                "Crunchy",
+                "Damp",
+                "Dull",
+                "Enchanted",
+                "Exciting",
+                "Fake",
+                "Feeble",
+                "Fierce",
+                "Floppy",
+                "Foolish",
+                "Fragile",
+                "Freaky",
+                "Friendly",
+                "Furry",
+                "Grumpy",
+                "Hairy",
+                "Inventive",
+                "Lumpy",
+                "Magical",
+                "Mushy",
+                "Quirky",
+                "Repulsive",
+                "Ruthless",
+                "Savory",
+                "Sharp",
+                "Silly",
+                "Slinky",
+                "Solid",
+                "Spicy",
+                "Squeaky",
+                "Sticky",
+                "Stiff",
+                "Strong",
+                "Tangy",
+                "Terrible",
+                "Tasty",
+                "Tricky",
+                "Weird",
+                "Wild",
+                "Wonderful",
+                "Yummy",
+                "Zesty"
+        };
 
         readonly string[] blueList = { "Wobbly", "Spikey", "Childish", "Lazy", "Wet", "Invisible"};
 
